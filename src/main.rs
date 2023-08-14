d70ed99126c24666895dd67f67761214fc47896c
yoshi-koyama <koyama@reytech.co.jp>
2023-08-14 14:03:39 +0900

    modify response as JSON

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,45 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::serde::json::Json;
+use rocket::serde::Serialize;
+
 #[get("/current-time")]
-fn index() -> String {
+fn current_time() -> Json<Response> {
     // 現在日付を返す
     let now = chrono::Local::now();
-    now.format("%Y-%m-%d %H:%M:%S").to_string()
+    Json(Response {
+        message: now.format("%Y-%m-%d %H:%M:%S").to_string(),
+    })
 }
 
 #[get("/hello")]
-fn hello() -> String {
-    "Hello, world!".to_string()
+fn hello() -> Json<Response> {
+    Json(Response {
+        message: "Hello, world!".to_string(),
+    })
+}
+
+#[get("/greeting?<country>")]
+fn greeting(country: Option<String>) -> Json<Response> {
+    let greeting = match country.as_deref() {
+        Some("japan") => "こんにちは",
+        Some("china") => "你好",
+        Some("us") => "Hello",
+        _ => "???",
+    };
+    Json(Response {
+        message: greeting.to_string(),
+    })
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![current_time, hello, greeting])
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Response {
+    message: String,
 }
